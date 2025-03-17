30a61b1d20c2ccf78c7c7891ffb7a6c7bbb95920
Lestat <2233915@slu.edu.ph>
2025-03-17 13:20:16 +0800

    initialize server module and add main application file

---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module amalzen.org/cache_game/server
+
+go 1.23.1
---
+++ server/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+func main() {
+	var variable string
+	variable = "Hello, World!"
+	fmt.Println(variable)
+	
+}
