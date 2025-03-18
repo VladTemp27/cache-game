8ef23a89438da1a88edb8e2063cd3a5fef4ba2e8
Marven <luis.marvenjoffre@gmail.com>
2025-03-19 03:58:51 +0800

    feat: add User model schema for card pairs

---
+++ server/rest-api/services/dal-service/models/Cards.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const UserFormat = new mongoose.Schema({
+    pair_id: { type: Number, required: true },
+    pair: [{
+        name: String,
+        question: String
+    }]
+});
+
+module.exports = mongoose.model("User", UserFormat);
