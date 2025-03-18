0f9546fb0c669f3aded71a21d8ac5f1263cfc53e
Gebreyl <2233944@slu.edu.ph>
2025-03-18 23:16:21 +0800

    + Modified index.js to be able to access the authentication service

--- server/rest-api/index.js
+++ server/rest-api/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const authRouter = require("./services/auth-service/auth");
 
 const app = express();
 app.use(express.json());
@@ -7,6 +8,8 @@ app.get('/health', (req,res) => {
     res.send({message : "Server is up and running"});
 })
 
+app.use('/auth', authRouter);
+
 app.listen(8080, () => {
-    console.log("Server is running on port 8000")
+    console.log("Server is running on port 8080")
 })
