4a3e93e23862f1a922ee1599ee502fc6c7151629
Sean Octavo <2220360@slu.edu.ph>
2025-03-19 02:03:21 +0800

    Create main menu controller

--- app/src/main/java/org/amalzen/app/main_menu/MainmenuView.java
+++ app/src/main/java/org/amalzen/app/main_menu/MainmenuView.java
@@ -4,14 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.amalzen.app.HelloApplication;
+import org.amalzen.app.main_menu.MainmenuView;
 
 import java.io.IOException;
 
 public class MainmenuView extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/mainmenu-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainmenuView.class.getResource("view/mainmenu-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
         stage.setTitle("Hello!");
         stage.setScene(scene);
