ac181981400b61d5d5bd43e538c8a0d675ac1de4
Sean Octavo <2220360@slu.edu.ph>
2025-03-19 16:34:32 +0800

    Created back button listener on game instructions

--- app/src/main/java/org/amalzen/app/game_instructions/GameInstructionsController.java
+++ app/src/main/java/org/amalzen/app/game_instructions/GameInstructionsController.java
@@ -2,6 +2,7 @@ package org.amalzen.app.game_instructions;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
@@ -14,6 +15,17 @@ public class GameInstructionsController {
     private Button backButton;
 
     public void initialize() {
-
+        backButton.setOnMouseClicked(event -> {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/amalzen/app/view/mainmenu-view.fxml"));
+                Parent gameInstructionsRoot = loader.load();
+                Stage stage = (Stage) backButton.getScene().getWindow();
+                Scene scene = new Scene(gameInstructionsRoot);
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
