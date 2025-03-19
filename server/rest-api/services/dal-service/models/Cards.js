14c20fd230abfa4e15d77b2b960b63e1eaeb24bc
Marven <luis.marvenjoffre@gmail.com>
2025-03-20 04:50:23 +0800

    feat: update card schema to include _id field

--- server/rest-api/services/dal-service/models/Cards.js
+++ server/rest-api/services/dal-service/models/Cards.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
-const CardFormat = new mongoose.Schema({
-    pair_id: { type: Number, required: true },
-    pair: {
-        answer: { type: String, required: true },
-        question: { type: String, required: true }
-    }
+const cardSchema = new mongoose.Schema({
+  _id: { type: mongoose.Schema.Types.ObjectId, required: true },
+  pair_id: { type: Number, required: true },
+  pair: {
+    answer: { type: String, required: true },
+    question: { type: String, required: true }
+  }
 });
 
-module.exports = mongoose.models.Card || mongoose.model("Card", CardFormat);
+module.exports = mongoose.model("Card", cardSchema);
