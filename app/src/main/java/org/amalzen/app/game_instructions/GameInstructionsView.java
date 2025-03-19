0954b44e0aad5614d891af3c2b3b77abf3ef3a89
Sean Octavo <2220360@slu.edu.ph>
2025-03-19 16:22:26 +0800

    Changed main menu and game instructions view

--- app/src/main/java/org/amalzen/app/game_instructions/GameInstructionsView.java
+++ app/src/main/java/org/amalzen/app/game_instructions/GameInstructionsView.java
@@ -11,7 +11,7 @@ public class GameInstructionsView extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GameInstructionsView.class.getResource("/org/amalzen/app/view/game_instructions.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+        Scene scene = new Scene(fxmlLoader.load(), 1650, 944);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
