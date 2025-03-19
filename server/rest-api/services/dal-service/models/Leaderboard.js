2c94b9138f41dee0247f31d68a81a9a1d4627254
Marven <luis.marvenjoffre@gmail.com>
2025-03-20 04:50:10 +0800

    feat: remove leaderboard service and related files

--- server/rest-api/services/dal-service/models/Leaderboard.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const LeaderboardFormat = new mongoose.Schema({
-    user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    total_score: { type: Number, required: true }
-});
-
-module.exports = mongoose.model("Leaderboard", LeaderboardFormat);
