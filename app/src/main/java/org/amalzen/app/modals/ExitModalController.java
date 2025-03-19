0ad94ddea0c8b0b9c96d3e9df673445d9a454f3b
Sean Octavo <2220360@slu.edu.ph>
2025-03-19 19:15:20 +0800

    Create exit confirmation modal

---
+++ app/src/main/java/org/amalzen/app/modals/ExitModalController.java
@@ -0,0 +1,16 @@
+package org.amalzen.app.modals;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ExitModalController {
+
+    @FXML
+    private Button cancelExitButton;
+
+    @FXML
+    private Button confirmExitButton;
+
+
+
+}
