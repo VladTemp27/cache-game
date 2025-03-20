f4cb44aa7ccfbe0d99746fcc4fb7a836a58756cc
Benny Gil Lactaotao <benny.gil999@gmail.com>
2025-03-20 11:59:57 +0800

    Revert "Authentication Service"

--- server/rest-api/index.js
+++ server/rest-api/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const authRouter = require("./services/auth-service/auth");
 
 const app = express();
 app.use(express.json());
@@ -8,8 +7,6 @@ app.get('/health', (req,res) => {
     res.send({message : "Server is up and running"});
 })
 
-app.use('/auth', authRouter);
-
 app.listen(8080, () => {
-    console.log("Server is running on port 8080")
+    console.log("Server is running on port 8000")
 })
