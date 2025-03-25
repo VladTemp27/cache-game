c51dd323d51eb1dbfbbf94eff247dcaf97e688cc
Lestat <2233915@slu.edu.ph>
2025-03-25 23:02:00 +0800

    refactor: enhance card flipping animation and add card label support

--- app/src/main/java/org/amalzen/app/components/CardComponent.java
+++ app/src/main/java/org/amalzen/app/components/CardComponent.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.scene.transform.Rotate;
 import javafx.util.Duration;
 
@@ -13,8 +14,8 @@ public class CardComponent {
     @FXML private StackPane cardStackPane;
     @FXML private ImageView cardBack;
     @FXML private ImageView cardFront;
-
-    // TODO: Add attributes for card data
+    @FXML private Text cardLabel;
+    @FXML private StackPane cardFaces;
 
 
     private boolean isFlipped = false;
@@ -34,25 +35,38 @@ public class CardComponent {
     }
 
     public void flipCard() {
-        RotateTransition rotateOut = new RotateTransition(Duration.millis(300), cardStackPane);
+        // prevents re-flipping of already flipped cards
+        if(isFlipped){
+            return;
+        }
+        RotateTransition rotateOut = new RotateTransition(Duration.millis(300), cardFaces);
         rotateOut.setAxis(Rotate.Y_AXIS);
         rotateOut.setFromAngle(0);
         rotateOut.setToAngle(90);
 
+        //Executes once initial flip is done
         rotateOut.setOnFinished(event -> {
             // Toggle image visibility at 90-degree mark
             cardFront.setOpacity(isFlipped ? 1 : 0);
             cardBack.setOpacity(isFlipped ? 0 : 1);
+            cardLabel.setOpacity(isFlipped ? 0 : 1);
             isFlipped = !isFlipped;
 
             // Rotate back from 90° to 180° to complete the flip
-            RotateTransition rotateIn = new RotateTransition(Duration.millis(300), cardStackPane);
+            RotateTransition rotateIn = new RotateTransition(Duration.millis(300), cardFaces);
             rotateIn.setAxis(Rotate.Y_AXIS);
             rotateIn.setFromAngle(90);
             rotateIn.setToAngle(180);
             rotateIn.play();
-        });
 
+            //Hotfix for card label
+            //FIXME:Since this is a temporary fix, the animation is a bit funky
+            RotateTransition rotateLabel = new RotateTransition(Duration.millis(300), cardLabel);
+            rotateLabel.setAxis(Rotate.Y_AXIS);
+            rotateLabel.setFromAngle(90);
+            rotateLabel.setToAngle(0);
+            rotateLabel.play();
+        });
         rotateOut.play();
     }
 }
