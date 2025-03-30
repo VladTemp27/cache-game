a08ccd2ee3c4d43a87b6a5b406065b15defd005e
Sean Octavo <2220360@slu.edu.ph>
2025-03-30 20:40:40 +0800

    add: leaderboard ui

---
+++ app/src/main/java/org/amalzen/app/components/LeaderboardComponent.java
@@ -0,0 +1,11 @@
+package org.amalzen.app.components;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class LeaderboardComponent {
+
+    @FXML
+    private AnchorPane componentRootPane;
+
+}
---
+++ app/src/main/java/org/amalzen/app/leaderboards/LeaderboardController.java
@@ -0,0 +1,17 @@
+package org.amalzen.app.leaderboards;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class LeaderboardController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private VBox leaderboardUserContainer;
+
+}
