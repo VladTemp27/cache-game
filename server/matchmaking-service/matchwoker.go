53cdbceb096dd7be94fa8912c00aa6213ab57102
Lestat <2233915@slu.edu.ph>
2025-03-30 23:36:28 +0800

    Add clarification comment on timeout cutoff calculation in removeTimedOutPlayers

--- server/matchmaking-service/matchwoker.go
+++ server/matchmaking-service/matchwoker.go
@@ -394,6 +394,7 @@ func (w *MatchWorker) notifyAndCloseConnection(client *Client, match Match, oppo
 
 func (w *MatchWorker) removeTimedOutPlayers() error{
     // Calculate the cutoff time (30 seconds ago)
+    // Note since the tick rate is 5 seconds this will cut off on 35 seconds in actuality
     timeoutCutoff := time.Now().Add(-30 * time.Second)
         
     ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
