b51727b69ec010d3f3a005664ba6647860e04bb9
Marven <luis.marvenjoffre@gmail.com>
2025-04-03 04:02:16 +0800

    used verify user

--- server/rest-api/services/dal-service/routes/gameHistoryRoutes.js
+++ server/rest-api/services/dal-service/routes/gameHistoryRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { getAllGameHistories, getGameHistoryById, upsertGameHistory, updateGameHistoryJSON } = require('../data-service/gameHistoryService');
-const { upsertUser } = require('../data-service/userService');
+const { upsertUser, verifyUser } = require('../data-service/userService');
 
-// Get all game histories
 router.get('/getAllGameHistories', async (req, res) => {
     try {
         const gameHistories = await getAllGameHistories();
@@ -13,12 +12,10 @@ router.get('/getAllGameHistories', async (req, res) => {
     }
 });
 
-// Get a specific game history by ID
 router.get('/getGameHistoryById/:id', getGameHistory, (req, res) => {
     res.json(res.gameHistory);
 });
 
-// Create a new game history
 router.post('/upsertGameHistory', async (req, res) => {
     const gameHistoryData = {
         gameroom_id: req.body.gameroom_id,
@@ -32,30 +29,13 @@ router.post('/upsertGameHistory', async (req, res) => {
     try {
         const newGameHistory = await upsertGameHistory(gameHistoryData);
         await updateGameHistoryJSON();
-
-        if (gameHistoryData.player_1){            
-            const player_1 = {
-                username: gameHistoryData.player_1,
-                score: gameHistoryData.player_1_score
-            }
-            await upsertUser(player_1)
-        }
-
-        if (gameHistoryData.player_2){
-            const player_2 = {
-                username: gameHistoryData.player_2,
-                score: gameHistoryData.player_2_score
-            }
-            await upsertUser(player_2)
-        }
-
+        
         res.status(201).json(newGameHistory);
     } catch (err) {
         res.status(400).json({ message: err.message });
     }
 });
 
-// Middleware to get game history by ID
 async function getGameHistory(req, res, next) {
     let gameHistory;
     try {
